8d7ad73205d541f0d8b8708cd4a9ae8bcf8fec6f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-05 16:11:57 -0300

    feat: add loader and products

--- list-search/case-0/src/App.tsx
+++ list-search/case-0/src/App.tsx
@@ -2,29 +2,33 @@ import type {Product} from "./types";
 
 import {useEffect, useState} from "react";
 
-import api from "./api";
+import {getProducts} from "./services/getProducts";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
 
   useEffect(() => {
-    api.search(query);
+    getProducts(query).then(setProducts);
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
-      </ul>
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      {products.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id} className={`${product.price <= 100 ? "sale" : ""}`}>
+              <h4>{product.title}</h4>
+              <p>{product.description}</p>
+              <span>$ {product.price}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
