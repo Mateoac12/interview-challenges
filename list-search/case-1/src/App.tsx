543d42f6ae72fb758fd5bb228d770a7971cccd51
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-05 17:49:48 -0300

    feat: search products

--- list-search/case-1/src/App.tsx
+++ list-search/case-1/src/App.tsx
@@ -1,27 +1,64 @@
 import type {Product} from "./types";
 
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 
 import api from "./api";
+import {getPrice} from "./utils/getPrice";
+
+enum SortOptions {
+  NAME = "name",
+  PRICE = "price",
+}
+
+const initialQuery = () => localStorage.getItem("query") || "";
+const initialSort = () => (localStorage.getItem("sort") as SortOptions) || SortOptions.NAME;
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
-  const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>(initialQuery);
+  const [sort, setSort] = useState<SortOptions>(initialSort);
 
   useEffect(() => {
     api.search(query).then(setProducts);
   }, [query]);
 
+  useEffect(() => {
+    window.localStorage.setItem("query", query);
+  }, [query]);
+
+  useEffect(() => {
+    window.localStorage.setItem("sort", sort);
+  }, [sort]);
+
+  const sortProducts = useMemo(() => {
+    if (sort === SortOptions.NAME) products.sort((a, b) => a.title.localeCompare(b.title));
+    if (sort === SortOptions.PRICE) products.sort((a, b) => a.price - b.price);
+
+    return products;
+  }, [sort, products]);
+
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <div>
+        <input
+          name="text"
+          placeholder="tv"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <select value={sort} onChange={(e) => setSort(e.target.value as SortOptions)}>
+          <option value={SortOptions.NAME}>📖 Nombre</option>
+          <option value={SortOptions.PRICE}>💵 Precio</option>
+        </select>
+      </div>
       <ul>
-        {products.map((product) => (
+        {sortProducts.map((product) => (
           <li key={product.id}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
-            <span>$ {product.price}</span>
+            <span>{getPrice(product.price)}</span>
           </li>
         ))}
       </ul>
