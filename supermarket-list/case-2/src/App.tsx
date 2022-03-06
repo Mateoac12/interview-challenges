72ac72da45d041c7c60331bb407fee2e64c9ef24
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-06 16:01:42 -0300

    fix exercise

--- supermarket-list/case-2/src/App.tsx
+++ supermarket-list/case-2/src/App.tsx
@@ -5,26 +5,41 @@ import {useEffect, useState} from "react";
 import styles from "./App.module.scss";
 import api from "./api";
 
+/* 
+## Nivel 1
+- [] Podemos agregar elementos vacíos
+*/
 interface Form extends HTMLFormElement {
   text: HTMLInputElement;
 }
 
+const initialInputState = "";
+
 function App() {
   const [items, setItems] = useState<Item[]>([]);
+  const [input, setInput] = useState<string>(initialInputState);
   const [isLoading, toggleLoading] = useState<boolean>(true);
 
   function handleToggle(id: Item["id"]) {
     // Should implement
+    const newItems = items.map((item) => ({
+      ...item,
+      completed: item.id === id ? !item.completed : item.completed,
+    }));
+
+    setItems(newItems);
   }
 
   function handleAdd(event: React.ChangeEvent<Form>) {
     event.preventDefault();
 
+    if (input.trim() === "") return;
+
     setItems((items) =>
       items.concat({
         id: +new Date(),
         completed: false,
-        text: event.target.text.value,
+        text: input,
       }),
     );
 
@@ -36,10 +51,14 @@ function App() {
   }
 
   useEffect(() => {
-    api
-      .list()
-      .then(setItems)
-      .finally(() => toggleLoading(false));
+    const time = setTimeout(() => {
+      api
+        .list()
+        .then(setItems)
+        .finally(() => toggleLoading(false));
+    }, 1000);
+
+    return () => clearTimeout(time);
   }, []);
 
   if (isLoading) return "Loading...";
@@ -48,7 +67,7 @@ function App() {
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form onSubmit={handleAdd}>
-        <input name="text" type="text" />
+        <input name="text" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
         <button>Add</button>
       </form>
       <ul>
